5c48d0191be2327e74b85ca0e4eaa4f00a3a67a9
Augustin Anders <augustinanders@gmail.com>
2023-06-01 12:17:02 +0200

    basic setup

---
+++ src/ui/Header.js
@@ -0,0 +1,10 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>test</Toolbar>
+    </AppBar>
+  );
+}
